91359fbe308ccd964178cadd4b708806510a4200
datta <dattu99rockstar@gmail.com>
2024-08-24 09:53:29 +0530

    may be web

--- lib/Services/weather_service.dart
+++ lib/Services/weather_service.dart
@@ -1,17 +1,28 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 class WeatherService {
   final String apiKey = 'c6586a42afeb4c3ca3141816240408'; // Your API key for WeatherAPI
   final String baseUrl = 'http://api.weatherapi.com/v1/current.json';
+  final Dio _dio = Dio();  // Create a Dio instance
 
   Future<Map<String, dynamic>> fetchWeather(String city) async {
-    final response = await http.get(Uri.parse('$baseUrl?key=$apiKey&q=$city&aqi=no'));
+    try {
+      final response = await _dio.get(
+        baseUrl,
+        queryParameters: {
+          'key': apiKey,
+          'q': city,
+          'aqi': 'no',
+        },
+      );
 
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      throw Exception('Failed to load weather data');
+      if (response.statusCode == 200) {
+        return response.data as Map<String, dynamic>;
+      } else {
+        throw Exception('Failed to load weather data');
+      }
+    } catch (e) {
+      throw Exception('Failed to load weather data: $e');
     }
   }
 }
